116d58cc8b34f6dc01f8056953e54c3d0e907d5c
Alex Naga <alex.naga@tiqqe.com>
2019-09-16 13:34:50 +0200

    Possibility to send a message through WebSocket

--- src/pushNotifications/pushNotifications.js
+++ src/pushNotifications/pushNotifications.js
@@ -1,49 +1,57 @@
 const AWS = require('aws-sdk');
-const parseDynamoDBNewImageEvent = require('./parseDynamodbNewImageEvent');
 
 // Add ApiGatewayManagementApi to the AWS namespace
 require('aws-sdk/clients/apigatewaymanagementapi');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const { wsClientsTable, stage, apiEndpoint } = process.env;
-const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+const apiGatewayManagementApi = new AWS.ApiGatewayManagementApi({
   apiVersion: '2018-11-29',
   endpoint: `${apiEndpoint}/${stage}`,
 });
 
-function pushNotification(clientId, notificationData) {
-  return apigwManagementApi.postToConnection({
-    ConnectionId: clientId, Data: JSON.stringify(notificationData),
-  }).promise()
-    .catch((errorPosting) => {
-      if (errorPosting.statusCode === 410) {
-        console.log(`Found stale connection, deleting ${clientId}`);
-        return dynamoDb.delete({
-          TableName: wsClientsTable, Key: { clientId },
-        }).promise();
-      }
-
-      throw errorPosting;
-    });
+async function pushNotification(clientId, message) {
+  try {
+    return apiGatewayManagementApi.postToConnection({
+      ConnectionId: clientId,
+      Data: message,
+    }).promise();
+  } catch (errorPosting) {
+    if (errorPosting.statusCode === 410) {
+      console.log(`Found stale connection, deleting ${clientId}`);
+      return dynamoDb.delete({
+        TableName: wsClientsTable, Key: { clientId },
+      }).promise();
+    }
+    throw errorPosting;
+  }
 }
 
 function processResponse(statusCode, data) {
   return { statusCode, body: JSON.stringify(data) };
 }
 
-exports.handler = (event) => {
-  const newEvents = parseDynamoDBNewImageEvent(event);
+exports.handler = async (event) => {
+  const { action } = event.body;
+  const bodyIsObj = typeof event.body === 'object';
 
-  // TODO: Don't use scan, use query instead
-  return dynamoDb.scan({
-    TableName: wsClientsTable, ProjectionExpression: 'clientId',
-  }).promise()
-    .then((result) => {
-      const postCalls = result.Items.map(({ clientId }) => pushNotification(clientId, newEvents));
-      return Promise.all(postCalls);
-    }).then(() => processResponse(200, 'Data sent'))
-    .catch((err) => {
+  let message = event.body;
+  if (bodyIsObj) {
+    message = JSON.stringify(event.body.data);
+  }
+
+  if (action && action === 'sendMessage') {
+    // TODO: Don't use scan, use query instead since we want to send to a specific user
+    try {
+      const result = await dynamoDb.scan({
+        TableName: wsClientsTable, ProjectionExpression: 'clientId',
+      }).promise();
+      const postCalls = result.Items.map(({ clientId }) => pushNotification(clientId, message));
+      await Promise.all(postCalls);
+      return processResponse(200, 'Data sent');
+    } catch (err) {
       console.log(err);
       return processResponse(500, err.stack);
-    });
+    }
+  }
 };
